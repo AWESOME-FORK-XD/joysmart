9988ce78447a1cd0a35ba711c19bdfd245d1e168
joysmartV2 <123448788+joysmartV2@users.noreply.github.com>
2023-01-25 09:34:33 +0800

    Update addip.sh

--- menu/addip.sh
+++ menu/addip.sh
@@ -20,7 +20,7 @@ CYAN='\e[36m'
 LIGHT='\033[0;37m'
 tokengit=$(cat /etc/adminip/access.conf)
 MYIP=$(wget -qO- ipinfo.io/ip);
-adminip=$( curl -sS https://raw.githubusercontent.com/joysmartV2/allow/main/accessmenu | grep $MYIP )
+adminip=$( curl -sS https://raw.githubusercontent.com/joysmartV2/allow/main/adminipvps | grep $MYIP )
 if [ $adminip = $MYIP ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
